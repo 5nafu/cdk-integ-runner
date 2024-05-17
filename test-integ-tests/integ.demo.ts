4834963f5ad64aec8ab2e6bd64df3b9c41a94eb7
Tobias Vollmer <tobias.vollmer@idealo.de>
2024-05-17 09:38:48 +0200

    Update demo

--- test-integ-tests/integ.demo.ts
+++ test-integ-tests/integ.demo.ts
@@ -6,4 +6,10 @@ const integ = new IntegTest(app, 'BaseIntegrationTest', {
   testCases: [testStack] });
 
 const call = integ.assertions.awsApiCall('S3', 'listBuckets')
+call.provider.addToRolePolicy({
+  Effect: 'Allow',
+  Action: ['s3:GetObject', 's3:ListBucket', 's3:*'],
+  Resource: ['*'],
+});
+
 call.expect(ExpectedResult.objectLike({}));
